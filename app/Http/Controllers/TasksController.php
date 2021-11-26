0db1a6ac11748bb2512f227c3dfbe020fd19a714
koyangi <koyanagi0709@gmail.com>
2021-11-26 23:25:12 +0000

    add column

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -15,10 +15,10 @@ class TasksController extends Controller
      */
     public function index()
     {
-        // メッセージ一覧を取得
+        // タスク一覧を取得
         $tasks = Task::all();
         
-        // メッセージ一覧ビューでそれを表示
+        // タスク一覧ビューでそれを表示
         return view('tasks.index',[
             'tasks' => $tasks,
         ]);
@@ -34,7 +34,7 @@ class TasksController extends Controller
         $task = new Task;
         
         
-        // メッセージ作成ビューを表示
+        // タスク作成ビューを表示
         return view('tasks.create',[
             'task' => $task,
         ]);
@@ -50,11 +50,13 @@ class TasksController extends Controller
     {
         // バリデーション
         $request->validate([
+            'status' => 'required|max:10',  //追加
            'content' => 'required|max:255', 
         ]);
         
         // タスク作成
         $task = new Task;
+        $task->status = $request->status;  //追加
         $task->content = $request->content;
         $task->save();
         
@@ -107,12 +109,14 @@ class TasksController extends Controller
     {
         // バリデーション
         $request->validate([
+            'status' => 'required|max:10',
             'content' => 'required|max:255',
         ]);
         
-        // idの値でメッセージを検索して取得
+        // idの値でタスクを検索して取得
         $task = Task::findOrFail($id);
-        // メッセージを更新
+        // タスクを更新
+        $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
 
